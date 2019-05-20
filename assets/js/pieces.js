4a170b47e318c12d0e5f5174bd9570b6a2a8a2ca
Philip Telkamp <philiptelkamp@gmail.com>
2019-05-20 10:00:26 -0500

    Fixed javascript object notation, added king piece

--- assets/js/pieces.js
+++ assets/js/pieces.js
@@ -18,7 +18,7 @@ class ChessPiece {
      * @param {in} y 
      * @param {in} board 
      * 
-     * @returns Oject array of each potential coodinate for a move form [{ "x": val, "y": val}]
+     * @returns Oject array of each potential coodinate for a move form [{ x: val, y: val}]
      */
     getPotentialMoves(x, y, board) {
         console.log("getPossibleMoves not implemented");
@@ -47,10 +47,10 @@ class Bishop extends ChessPiece {
     getPotentialMoves(x, y, board) {
         let moves = [];
         let i = 1;
-
+        
         //Iterate through each coordinate quadrant, centered on piece stopping
         let stop = false;
-
+        
         // Quadrant I
         while(x + i < 8 && y + i < 8 && !stop)
         {
@@ -60,14 +60,14 @@ class Bishop extends ChessPiece {
             }
 
             moves.push({
-                "x" : x + i,
-                "y" : y + i
+                x : x + i,
+                y : y + i
             });
         }
-
+        
         i = 1;
         stop = false;
-
+        
         // Quadrant II
         while(x - i >= 0 && y + i < 8 && !stop)
         {
@@ -75,16 +75,16 @@ class Bishop extends ChessPiece {
             {
                 stop = true;
             }
-
+            
             moves.push({
-                "x" : x - i,
-                "y" : y + i
+                x : x - i,
+                y : y + i
             });
         }
-
+        
         i = 1;
         stop = false;
-
+        
         // Quadrant III
         while(x - i >= 0 && y - i >= 0 && !stop)
         {
@@ -92,17 +92,17 @@ class Bishop extends ChessPiece {
             {
                 stop = true;
             }
-
+            
             moves.push({
-                "x" : x - i,
-                "y" : y - i
+                x : x - i,
+                y : y - i
             });
         }
-
-
+        
+        
         i = 1;
         stop = false;
-
+        
         // Quadrant IV
         while(x + i < 8 && y - i >= 0 && !stop)
         {
@@ -110,11 +110,36 @@ class Bishop extends ChessPiece {
             {
                 stop = true;
             }
-
+            
             moves.push({
-                "x" : x + i,
-                "y" : y - i
+                x : x + i,
+                y : y - i
             });
+        }
+        
+        return moves;
+    }
+
+
+}
+
+class King extends ChessPiece {
+    constructor(isWhite, display) {
+        super(isWhite, display, "K");
+    }
+
+    getPotentialMoves(x, y, board) {
+        let moves = [];
+
+        for(let col = x - 1; col <= x + 1; col++) {
+            for( let row = y - 1; row <= y + 1; row++) {
+                if(row >= 0 && row < 8 && col >= 0 && row < 8) {
+                    moves.push({
+                        x : col,
+                        y : row 
+                    });
+                }
+            }
         }
 
         return moves;
@@ -122,36 +147,37 @@ class Bishop extends ChessPiece {
 }
 
 class Pawn extends ChessPiece {
+
     constructor(isWhite, display) {
         super(isWhite, display, "P");
     }
-
+    
     getPotentialMoves(x, y, board) {
         let moves = [];
-
+        
         if(this.isWhite) {
             moves.push({
-                "x" : x,
-                "y" : y - 1 
+                x : x,
+                y : y - 1 
             });
 
             if(y == 6 && !board[x][5]) {
                 moves.push({ 
-                    "x" : x,
-                    "y" : y - 2 
+                    x : x,
+                    y : y - 2 
                 });
             }
         }
         else {
             moves.push({
-                "x" : x,
-                "y" : y + 1
+                x : x,
+                y : y + 1
             });
 
             if(y == 1 && !board[x][2]) {
                 moves.push({
-                    "x" : x,
-                    "y" : y + 2
+                    x : x,
+                    y : y + 2
                 });
             }
         }
